64b3b087107496269443e8434f11671b825b8d3a
kandalog <asuasu.tee@gmail.com>
2023-03-13 20:39:39 +0900

    Fast API 公式ドキュメント SQL完

---
+++ src/schemas.py
@@ -0,0 +1,36 @@
+from pydantic import BaseModel
+
+
+class ItemBase(BaseModel):
+    title: str
+    description: str | None = None
+
+
+class ItemCreate(ItemBase):
+    pass
+
+
+class Item(ItemBase):
+    id: int
+    owner_id: int
+
+# ORMから返ってきた場合も読み取るようにする(ORMから返る構造は単純なdicじゃない)
+    class Config:
+        orm_mode = True
+
+
+class UserBase(BaseModel):
+    email: str
+
+
+class UserCreate(UserBase):
+    password: str
+
+
+class User(UserBase):
+    id: int
+    is_active: bool
+    items: list[Item] = []
+
+    class Config:
+        orm_mode = True
